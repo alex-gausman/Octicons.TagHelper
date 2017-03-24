fb602e27f348eb14ca3f20bd007360be7880a4d6
Alex Gausman <alex.gausman@gmail.com>
2017-03-23 22:03:02 -0400

    initial commit

---
+++ Octicons.TagHelper/OcticonSymbol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Octicons.TagHelper
+{
+    internal class OcticonSymbol
+    {
+        public string Name { get; set; }
+        public string[] Keywords { get; set; }
+        public string Path { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
---
+++ Octicons.TagHelper/OcticonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TagHelpers = Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Octicons.TagHelper
+{
+    public class OcticonTagHelper : TagHelpers.TagHelper
+    {
+        private string _spriteSheet;
+        private List<OcticonSymbol> _octiconSymbols;
+
+        public OcticonTagHelper()
+        {
+            //File.ReadAllText(Directory.GetCurrentDirectory())
+        }
+
+        public override void Process(TagHelpers.TagHelperContext context, TagHelpers.TagHelperOutput output)
+        {
+            output.TagName = "a";
+        }
+    }
+}
