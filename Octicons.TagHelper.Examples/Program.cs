0a369d383eeed7148b1f56397afcc680a1c51fcc
FabriDamazio <fabridamazio@gmail.com>
2019-05-10 08:04:53 -0300

    Move to .NET 2.2

--- Octicons.TagHelper.Examples/Program.cs
+++ Octicons.TagHelper.Examples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Octicons.TagHelper.Examples
@@ -7,14 +8,12 @@ namespace Octicons.TagHelper.Examples
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
